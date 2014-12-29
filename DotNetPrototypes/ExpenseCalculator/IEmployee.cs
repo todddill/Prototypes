f76f3971b10ec0d1998e9492251bbb6d3d7b0c5c
Todd Dill <todd@tetranetic.com>
2014-12-29 10:57:32 -0600

    Expense Calculator Sample App

---
+++ DotNetPrototypes/ExpenseCalculator/IEmployee.cs
@@ -0,0 +1,8 @@
+﻿namespace ExpenseCalculator
+{
+    public interface IEmployee
+    {
+        string Title { get; set; }
+        decimal ExpenseAllocation { get; set; }
+    }
+}
---
+++ DotNetPrototypes/ExpenseCalculatorTests/ExpenseCalculatorTestHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExpenseCalculatorTests
+{
+    public class ExpenseCalculatorTestHelper
+    {
+        public List<Tuple<string, decimal>> Employees = new List<Tuple<string, decimal>>
+        {
+            new Tuple<string, decimal>("QA Tester", 500),
+            new Tuple<string, decimal>("Developer", 1000),
+            new Tuple<string, decimal>("Manager", 300)
+        };
+
+        public decimal GetTotalExpenses()
+        {
+            decimal expenses = 0;
+            foreach (Tuple<string, decimal> employee in this.Employees)
+            {
+                expenses += employee.Item2;
+            }
+            return expenses;
+        }
+    }
+}
