afd36d4b36410d1f1d707b3c85b52c6c8850e12a
Todd Dill <todd@tetranetic.com>
2014-12-29 12:11:43 -0600

    Added missing classes

---
+++ DotNetPrototypes/ExpenseCalculator/Employee.cs
@@ -0,0 +1,27 @@
+﻿namespace ExpenseCalculator
+{
+    public class Employee : IEmployee
+    {
+        #region IEmployee Members
+
+        public string Title
+        {
+            get;
+            set;
+        }
+
+        public decimal ExpenseAllocation
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
+        public Employee(string title, decimal expenseAllocation)
+        {
+            this.Title = title;
+            this.ExpenseAllocation = expenseAllocation;
+        }
+    }
+}
